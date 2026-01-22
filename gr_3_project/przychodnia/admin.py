2df842be629591f6dc64382cfa8904b6a60c76df
Alicja <aserafinska1@st.swps.edu.pl>
2026-01-22 15:57:37 +0100

    mini corrections

--- gr_3_project/przychodnia/admin.py
+++ gr_3_project/przychodnia/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 
 from .models import Opiekun, Zwierze, Weterynarz, GrafikDostepnosci, Wizyta
 
-@admin.register(Weterynarz)
-class WeterynarzAdmin(admin.ModelAdmin):
-    list_display = ('user', 'imie', 'nazwisko')
-    search_fields = ('user__drkowalski',)
-
 @admin.register(Opiekun)
 class OpiekunAdmin(admin.ModelAdmin):
     list_display = ('user', 'imie', 'nazwisko')
     search_fields = ('user__anna',)
+
+@admin.register(Weterynarz)
+class WeterynarzAdmin(admin.ModelAdmin):
+    list_display = ('user', 'imie', 'nazwisko')
+    search_fields = ('user__drkowalski',)
 
 admin.site.register(Zwierze)
 admin.site.register(GrafikDostepnosci)
